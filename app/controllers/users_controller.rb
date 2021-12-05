14a920b3e26fd3425f37b1c3f0c12b82e1ed17f3
EC2 Default User <ec2-user@ip-172-31-79-174.ec2.internal>
2021-12-05 02:46:55 +0000

    [Update] ファイルの記述・CRUDの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,28 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+    @user = User.find(current_user.id)
+  end
+
   def show
     @user = User.find(params[:id])
+    @books = Book.where(user_id: @user.id).includes(:user).order("created_at ASC")
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
 end
